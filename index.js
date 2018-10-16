3db5f42d8a57ca7cf97b0faaa5b3f44a14ac6850
jonsung89 <jonsung89@gmail.com>
2018-10-16 12:10:29 -0500

    Debugging - Issue with cors

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ const usersRouter = require('./routes/users');
 // Create Express Application
 const app = express();
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
